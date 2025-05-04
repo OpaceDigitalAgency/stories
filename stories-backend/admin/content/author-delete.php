b68f151c4c98cd135ab56d8cd2c87ac9b6c385d2
David Bryan <david@opace.co.uk>
2025-05-04 10:22:50 +0100

    fix: Move session_start() to beginning of author-delete.php to prevent headers already sent error

--- stories-backend/admin/content/author-delete.php
+++ stories-backend/admin/content/author-delete.php
@@ -1,4 +1,8 @@
 <?php
+// Start session if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Page variables
 $pageTitle = 'Author Delete';
@@ -13,11 +17,6 @@ require_once '../includes/db-connect.php';
 
 // Include header - must be after auth check and db connection
 require_once '../includes/header.php';
-
-// Start session if not already started
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 
 // Get author details
 $authorId = $_GET['id'] ?? 0;
